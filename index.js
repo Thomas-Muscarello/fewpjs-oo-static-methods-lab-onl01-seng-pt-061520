4ba5607f09a5ff88beaa832f0926e62ea7b45c50
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-01-19 19:39:27 -0500

    commit tree 05fd8870a849eede943a11e4adf8c9afaca3ca52

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(word) {
+    word = word.split(" ")
+    const bad = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    
+    for(let i = 0; i < word.length; i++) {
+      if (i === 0) {
+        word[i] = this.capitalize(word[i])
+      } else {
+        if (!(bad.includes(word[i]))) {
+          word[i] = this.capitalize(word[i])
+        }
+      }
+    }
+
+    return word.join(" ")
+  }
 }
